e9a4486271bba62773f98af030f7c46eae4e7722
hrrsnsu <hrrsnsu@gmail.com>
2026-02-02 20:14:08 -0800

    setup tables file

---
+++ setupDatabase.js
@@ -0,0 +1,52 @@
+import 'dotenv/config';
+import { neon } from '@neondatabase/serverless';
+
+const sql = neon(process.env.DATABASE_URL);
+
+async function setup() {
+    try {
+        console.log('Connection established');
+
+        //Create ufc_events table
+        await sql`DROP TABLE IF EXISTS ufc_events`;
+        await sql`
+			CREATE TABLE ufc_events (
+				id SERIAL PRIMARY KEY,
+				event_name VARCHAR(255) NOT NULL,
+				event_date DATE,
+				event_location VARCHAR(255)
+			);
+    	`;
+
+		//Create fighters table
+        await sql`DROP TABLE IF EXISTS fighters`;
+        await sql`
+			CREATE TABLE fighters (
+				id SERIAL PRIMARY KEY,
+				first_name VARCHAR(255) NOT NULL,
+				last_name VARCHAR(255),
+				age INT,
+				country VARCHAR(255),
+				height INT,
+				weight INT,
+				reach INT,
+				slmp DECIMAL,
+				strAcc DECIMAL,
+				sapm DECIMAL,
+				strDef DECIMAL,
+				tdAvg DECIMAL,
+				tdAcc DECIMAL,
+				tdDef DECIMAL,
+				subAvg DECIMAL,
+				next_event_id INT REFERENCES ufc_events(id),
+				last_event_id INT REFERENCES ufc_events(id)
+			);
+		`;
+
+		console.log('Finished creating tables.')
+    } catch (err) {
+        console.error('Connection failed.', err);
+    }
+}
+
+setup();
